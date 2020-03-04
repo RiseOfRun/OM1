26b347c2f52f05c7ec713f7a5566ca95b530d97c
Runi <runi54rus@gmail.com>
2020-03-04 15:39:13 +0700

    +Gold

--- OM1/Function.cs
+++ OM1/Function.cs
@@ -7,6 +7,7 @@ namespace OM1
 
         double a, b, delta, eps = 0.1, min;
         public double Min => min;
+        public int iterations = 0;
 
         public Function(double a, double b,double eps)
         {
@@ -22,28 +23,85 @@ namespace OM1
         {
             delta = eps / 4;
 
-
-            while (Math.Abs(Y(a)-Y(b))>eps)
+            double x1 = (a + b - delta) / 2;
+            double x2 = (a + b + delta) / 2;
+            double ya = Y(a);
+            double yb = Y(b);
+            double y1, y2;
+            double l = b - a;
+            while (Math.Abs(ya-yb)>eps)
             {
-                double x1 = (a + b - delta) / 2;
-                double x2 = (a + b + delta) / 2;
-                double y1 = Y(x1);
-                double y2 = Y(x2);
+                iterations++;
+                //Console.WriteLine($"{iterations} {a} {b} {l / (b - a)} {x1} {x2} {Y(x1)} {Y(x2)}");
+                x1 = (a + b - delta) / 2;
+                x2 = (a + b + delta) / 2;
+                y1 = Y(x1);
+                y2 = Y(x2);
                 if (y2>y1)
                 {
+                    l = b - a;
                     b = x2;
+                    yb = Y(x2);
                     continue;
                 }
                 if (y1>y2)
                 {
+                    l = b - a;
                     a = x1;
+                    ya = Y(x1);
                     continue;
                 }
+
                 a = x1;
                 b = x2;
+                ya = Y(x1);
+                yb = Y(x2);
             }
             min = Y((a + b) / 2);
+        }
 
+        public void Goden()
+        {
+            double r = (-1 + Math.Sqrt(5)) / 2;
+            double x1 = a+(1-r)*(b-a);
+            double l = b - a;
+            double x2 = a+r*(b-a);
+            double ya = Y(a);
+            double yb = Y(b);
+            double y1, y2;
+
+            while (Math.Abs(ya-yb)>eps)
+            {
+                iterations++;
+                //Console.WriteLine($"{iterations} {a} {b} {l/(b-a)} {x1} {x2} {Y(x1)} {Y(x2)}");
+                y1 = Y(x1);
+                y2 = Y(x2);
+                if (y1>y2)
+                {
+                    l = b - a;
+                    a = x1;
+                    x1 = x2;
+                    x2 = a + r * (b - a);
+                    ya = y1;
+                    y1 = y2;
+                    y2 = Y(x2);
+                    continue;
+                }
+
+                if (y1<y2)
+                {
+                    l = b - a;
+                    b = x2;
+                    x2 = x1;
+                    x1 = a + (1 - r) * (b - a);
+                    yb = y2;
+                    y2 = y1;
+                    y1 = Y(x1);
+                    continue;
+                }
+            }
+
+            min = Y((a + b) / 2);
         }
 
 
--- OM1/Program.cs
+++ OM1/Program.cs
@@ -6,9 +6,16 @@ namespace OM1
     {
         static void Main(string[] args)
         {
-            Function first = new Function(-10,20,0.001);
-            first.Dih();
-            Console.WriteLine($"{first.Min}");
+            double eps=1;
+            for (int i = 1; i < 8; i++)
+            {
+                eps /=10;
+                Function f1 = new Function(-10, 20, eps);
+                Function f2 = new Function(-10, 20, eps);
+                f1.Dih();
+                f2.Goden();
+                Console.WriteLine($"{eps} {f1.iterations*2} {f2.iterations+1}");
+            }
         }
     }
 }
